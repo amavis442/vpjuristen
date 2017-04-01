c050e6ca2b95ec598e77c70ead638fd59665fc41
Patrick Teunissen <patrick@yiiandme.com>
2017-04-02 00:32:20 +0200

    Added fillables for mass import

--- app/Role.php
+++ app/Role.php
@@ -21,6 +21,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Role extends Model
 {
+    protected $fillable = ['name','description'];
+
     public function users()
     {
         return $this->beloangsToMany('App\User');
