57e34c7e8c5feee6dbe8454c27d06865271cc921
Patrick Teunissen <patrick@yiiandme.com>
2017-04-01 18:27:34 +0200

    Grouped routes and added some middleware

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,20 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-// Client Frontend maybe put these in a group.
-Route::get('/aanmelding/', 'Frontend\ClientController@create')->name('client-create');
-Route::post('/aanmelding/store', 'Frontend\ClientController@store')->name('client-store');
+Route::group(['namespace' => 'Frontend', 'prefix' => 'registratie'], function () {
+    // Controllers Within The "App\Http\Controllers\Frontend" Namespace
+    Route::get('/', 'ClientController@create')->name('frontend.register.client.create');
+    Route::post('store', 'ClientController@store')->name('frontend.register.client.store');
+    Route::get('debtor', 'DebtorController@create')->name('frontend.register.debtor.create');
+    Route::post('debtor/store', 'DebtorController@store')->name('frontend.register.debtor.store');
+    Route::get('dossier', 'DossierController@create')->name('frontend.register.dossier.create');
+    Route::post('dossier/store', 'DossierController@store')->name('frontend.register.dossier.store');
+    Route::get('dossier/thankyou', 'DossierController@thankyou')->name('frontend.register.thankyou');
+});
 
-Route::get('/aanmelding/debtor', 'Frontend\DebtorController@create')->name('debtor-create');
-Route::post('/aanmelding/debtor/store', 'Frontend\DebtorController@store')->name('debtor-store');
 
-Route::get('/aanmelding/dossier', 'Frontend\DossierController@create')->name('dossier-create');
-Route::post('/aanmelding/dossier/store', 'Frontend\DossierController@store')->name('dossier-store');
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('dossier/view', 'Common\DossierController@view')->name('admin.dossier.list');
+    Route::get('dossier/create', 'Common\DossierController@create')->name('admin.dossier.create');
+});
+
