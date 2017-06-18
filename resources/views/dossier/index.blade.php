ee6dc0314869bfda02bc2ea631c4726695df21c2
Patrick Teunissen <patrick@yiiandme.com>
2017-06-18 13:14:53 +0200

    Made search option conditional depending on the user

--- resources/views/dossier/index.blade.php
+++ resources/views/dossier/index.blade.php
@@ -1,58 +1,49 @@
-@extends('layouts.app')
+@can('search')
+<div class="row">
 
-@section('content')
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h2>Dossier</h2>
-        </div>
-        <div class="panel-body">
-            <div class="row">
-                <div class="container">
-                    <form action="{{ url('admin/dossier/search') }}" method="get">
-                        <div class="form-group">
-                            <input
-                                    type="text"
-                                    name="q"
-                                    class="form-control"
-                                    placeholder="Search..."
-                                    value="{{ request('q') }}"
-                            />
-                        </div>
-                    </form>
-                </div>
+        <form action="{{ url($searchUrl) }}" method="get">
+            <div class="form-group">
+                <input
+                        type="text"
+                        name="q"
+                        class="form-control"
+                        placeholder="Search..."
+                        value="{{ request('q') }}"
+                />
             </div>
-            <div class="row">
-                <table class="table">
-                    <thead>
-                    <th>ID</th>
-                    <th>Description</th>
-                    <th>Client</th>
-                    <th>Debtor</th>
-                    <th>Last Action</th>
-                    <th>Status</th>
-                    <th>Created</th>
-                    <th>Invoices</th>
-                    </thead>
+        </form>
+</div>
+@endcan
 
-                    <tbody>
-                    @foreach($dossiers as $item)
-                        <tr>
-                            <td>#{{ $item->id }}</td>
-                            <td>
-                                <a href="{{ route('admin.dossier.show',['id' => $item->id]) }}">{{ $item->title }}</a>
-                            </td>
-                            <td>{{ $item->client()->first()->name  }}</td>
-                            <td>{{ $item->debtor()->first()->name  }}</td>
-                            <td>{{ ($item->actions()->count() > 0 ? $item->actions()->first()->title: '-')  }}</td>
-                            <td>{{ $item->dossierstatus()->first()->description  }}</td>
-                            <td> {{ $item->created_at }}</td>
-                            <td> {{ $item->updated_at }}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
+<div class="row">
+    <table class="table">
+        <thead>
+        <th>ID</th>
+        <th>Description</th>
+        <th>Client</th>
+        <th>Debtor</th>
+        <th>Last Action</th>
+        <th>Status</th>
+        <th>Created</th>
+        <th>Invoices</th>
+        </thead>
 
-                </table>
-            </div>
-        </div>
-    </div>
-@endsection
+        <tbody>
+        @foreach($dossiers as $item)
+            <tr>
+                <td>#{{ $item->id }}</td>
+                <td>
+                    <a href="{{ route($route,['id' => $item->id]) }}">{{ $item->title }}</a>
+                </td>
+                <td>{{ $item->client()->first()->name  }}</td>
+                <td>{{ $item->debtor()->first()->name  }}</td>
+                <td>{{ ($item->actions()->count() > 0 ? $item->actions()->first()->title: '-')  }}</td>
+                <td>{{ $item->dossierstatus()->first()->description  }}</td>
+                <td> {{ $item->created_at }}</td>
+                <td> {{ $item->updated_at }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+
+    </table>
+</div>
