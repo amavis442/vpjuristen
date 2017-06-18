ab27685358b309e9355f8ed783b6128313d70287
Patrick Teunissen <patrick@yiiandme.com>
2017-06-18 15:01:02 +0200

    Used policy to hide columns

--- resources/views/action/_list.blade.php
+++ resources/views/action/_list.blade.php
@@ -1,10 +1,12 @@
 <div class="col-md-12">
     <div class="panel panel-default">
         <div class="panel-heading">Actions
-            <div class="pull-right">
-                <a href="{{ route('admin.dossier.action.create', ['id' => $dossier->id]) }}">
-                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
-            </div>
+            @can('create', \App\Action::class)
+                <div class="pull-right">
+                    <a href="{{ route('admin.dossier.action.create', ['id' => $dossier->id]) }}">
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+                </div>
+            @endcan
         </div>
         <div class="panel-body">
             <table class="table">
@@ -14,17 +16,20 @@
                     <th>title</th>
                     <th>status</th>
                     <th>comment</th>
-                    <th>Visible for client</th>
-                    <th>Visible for debtor</th>
-                    <th>Amount</th>
-                    <th>created</th>
-                    <th>modified</th>
+                    @can('edit', \App\Action::class)
+                        <th>Visible for client</th>
+                        <th>Visible for debtor</th>
+
+                        <th>Amount</th>
+                        <th>created</th>
+                        <th>modified</th>
+                    @endcan
                     <th></th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach ($actions as $action)
-                    @if(Auth::user()->can('view',$action))
+                    @can('view', $action)
                         <?php
                         $meta = $actionMeta->get($action->id);
                         ?>
@@ -33,17 +38,21 @@
                             <td>{{ $action->title }}</td>
                             <td>{{ $meta->get('actionStatus') }}</td>
                             <td>{{ $meta->get('comment','') }}</td>
+                            @can('edit',\App\Action::class)
+                                <td>{{ $meta->get('clientCanSee') }}</td>
+                                <td>{{ $meta->get('debtorCanSee') }}</td>
 
-                            <td>{{ $meta->get('clientCanSee') }}</td>
-                            <td>{{ $meta->get('debtorCanSee') }}</td>
-                            <td>{{ $meta->get('amount') }}</td>
-                            <td>{{ $action->created_at->format('d-m-Y') }}</td>
-                            <td>{{ $action->updated_at->format('d-m-Y') }}</td>
+                                <td>{{ $meta->get('amount') }}</td>
+                                <td>{{ $action->created_at->format('d-m-Y') }}</td>
+                                <td>{{ $action->updated_at->format('d-m-Y') }}</td>
+                            @endcan
                             <td>
-                                <a href="{{ route('admin.dossier.action.edit', $action->id) }}">Bijwerken</a>
+                                @can('edit', $action)
+                                    <a href="{{ route('admin.dossier.action.edit', $action->id) }}">Bijwerken</a>
+                                @endcan
                             </td>
                         </tr>
-                    @endif
+                    @endcan
                 @endforeach
                 </tbody>
             </table>
