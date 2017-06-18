f5217d5f3d4461bb3ef43e981ebc8c49a3083daf
Patrick Teunissen <patrick@yiiandme.com>
2017-06-18 13:11:14 +0200

    Added logic for viewing invoice

--- app/Policies/InvoicePolicy.php
+++ app/Policies/InvoicePolicy.php
@@ -2,7 +2,9 @@
 
 namespace App\Policies;
 
+use App\Company;
 use App\Domain\Contract\UserInterface;
+use App\Dossier;
 use App\User;
 use App\Invoice;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -28,7 +30,19 @@ class InvoicePolicy
      */
     public function view(UserInterface $user, Invoice $invoice)
     {
-        //
+        /** @var Dossier $dossier */
+        $dossier = $invoice->dossier()->get()->first();
+        $clientId = $dossier->client_id;
+        /** @var Company $company */
+        $company = Company::findOrFail($clientId);
+        $companyUser = $company->users()->get()->first();
+
+        if ($companyUser->id == $user->id) {
+            return true;
+        }
+
+        return false;
+
     }
 
     /**
