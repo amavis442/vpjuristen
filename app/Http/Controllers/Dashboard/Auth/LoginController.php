05a026c63527bef4ed9c4d36cacd0e703a030060
Patrick Teunissen <patrick@yiiandme.com>
2017-06-28 18:47:33 +0200

    only redirect after login is usefull, rest is redundant.

--- app/Http/Controllers/Dashboard/Auth/LoginController.php
+++ app/Http/Controllers/Dashboard/Auth/LoginController.php
@@ -7,36 +7,11 @@ use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Auth;
 
-class LoginController extends Controller
+class LoginController extends \App\Http\Controllers\Auth\LoginController
 {
+    protected $redirectTo = '/dashboard/home';
 
-    public function __construct()
-    {
-        $this->redirectTo = route('dashboard.home');
-        $this->middleware('guest:dashboard', ['except' => 'logout']);
-    }
-
-
-    public function login(Request $request)
-    {
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required:min:6'
-        ]);
-
-        $credentials['email'] = $request->get('email');
-        $credentials['password'] = $request->get('password');
-        $remember = $request->get('remember');
-        if (Auth::guard('dashboard')->attempt($credentials, $remember)) {
-            $request->session()->regenerate();
-
-            return redirect()->intended(route('dashboard.home'));
-        }
-
-        return redirect()->back()->withInput($request->only('email', 'remember'));
-    }
-
-    public function showLoginForm(Request $request)
+    public function showLoginForm()
     {
         return view('dashboard.auth.login');
     }
