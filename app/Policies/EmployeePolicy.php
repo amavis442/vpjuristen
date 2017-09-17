918aa632d35108d5088fd6610d8e9090323026cc
Patrick Teunissen <p.teunissen18@gmail.com>
2017-09-17 13:11:07 +0200

    Cleanup policy

--- app/Policies/EmployeePolicy.php
+++ app/Policies/EmployeePolicy.php
@@ -3,7 +3,6 @@
 namespace App\Policies;
 
 use App\Models\User;
-use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class EmployeePolicy
@@ -37,7 +36,7 @@ class EmployeePolicy
      * Determine whether the user can update the user.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\User  $user
+     * @param  \App\Models\User  $user
      * @return mixed
      */
     public function update(User $user, User $employee)
@@ -49,7 +48,7 @@ class EmployeePolicy
      * Determine whether the user can delete the user.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\User  $user
+     * @param  \App\Models\User  $user
      * @return mixed
      */
     public function delete(User $user, User $employee)
