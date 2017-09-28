fc6f4622dbc8586a9656f5e64281dc5c07acb65f
Patrick Teunissen <p.teunissen18@gmail.com>
2017-09-28 22:54:51 +0200

    Added test for frontend dossier reg (client)

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -7,6 +7,7 @@ use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 use App\Models\Company;
+use App\Models\Contact;
 
 class LoginTest extends DuskTestCase
 {
@@ -19,12 +20,31 @@ class LoginTest extends DuskTestCase
      */
     public function testExample()
     {
+        /** @var Company $company */
         $company = factory(Company::class)->create();
+        $contact = factory(Contact::class)->create();
 
-        $this->browse(function (Browser $browser) {
+        $this->browse(function (Browser $browser) use ($company, $contact) {
             $browser->visit('/')
                     ->clickLink('Dossier aanmelden')
-            ->assertSee('Zipcode');
+                    ->type('company[name]', $company->name)
+                    ->type('company[street]', $company->street)
+                    ->type('company[housenr]', $company->housenr)
+                    ->type('company[postcode]', $company->postcode)
+                    ->type('company[city]', $company->city)
+                    ->type('company[country]', $company->country)
+                    ->type('company[phone]', $company->phone)
+                    ->type('company[email]', $company->email)
+                    ->type('company[website]', $company->website)
+
+                    ->radio('contact[sexe]', 'M')
+                    ->type('contact[firstname]', $contact->firstname)
+                    ->type('contact[middlename]', $contact->middlename)
+                    ->type('contact[name]', $contact->name)
+                    ->type('contact[phone]', $contact->phone)
+                    ->type('contact[email]', $contact->email)
+                    ->click('#btnSubmit')
+                    ->assertSee('Zipcode');
         });
     }
 }
